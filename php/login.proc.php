1bc4d160e17129bced88734cfb993c6e3e142576
mlopezgalan <lopezgalan.miguel@gmail.com>
2016-11-03 18:37:50 +0100

    Redirección en caso de login fallido en login.proc.php y salida de mensaje en index.php

--- php/login.proc.php
+++ php/login.proc.php
@@ -1,11 +1,23 @@
 <?php
 extract($_POST);
-echo "Tu nombre es: " .$name;
-echo "<br/> Tu contraseña es " . $pass;
-$mysqli = new mysqli("localhost", "root", "", "bd_mamproyecto2");
-	//formateamos el campo fecha, para que siga el estandar americano
 
-	if ($mysqli->connect_errno) {
+//Realizamos la conexión a la BD
+$mysqli = new mysqli("localhost", "root", "", "bd_mamproyecto2");
+if ($mysqli->connect_errno) {
     echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+	}
+//Consulta de busqueda del usuario
+	$con =	"SELECT * FROM `tbl_usuario` WHERE `usu_nickname` = '". $name ."' AND `usu_contrasena` = '" . $pass . "'";	
+	//Lanzamos la consulta a la BD
+	$result	=	mysqli_query($mysqli,$con);
+	//Contamos los resultados que nos devuelve
+	$total  = mysqli_num_rows($result); 
+	if($total>=1){
+		echo "Tu nombre es: " .$name;
+		echo "<br/> Tu contraseña es " . $pass;
+	}
+	else{
+		//echo "Tus datos no son correctos";
+		header("location: ../index.php?nolog=1");
 	}
 ?>

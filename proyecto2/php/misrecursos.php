676cf0de2227fdd3518bdcf1b40c08c4a904d738
Aaron4786 <4786.joan23@fje.edu>
2016-11-07 16:31:25 +0100

    Add files via upload

---
+++ proyecto2/php/misrecursos.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="../css/recursos.css">
+		<title>Recursos</title>
+		<script type="text/javascript">
+		function destroy(){
+			var respuesta = confirm("¿Está seguro que desea liberar este recurso?");
+			if(respuesta){
+				return true;
+			}
+			else{
+				return false;
+			}
+			
+		}
+			
+		</script>
+	</head>
+	<body>
+	<?php
+	session_start();
+	$mysqli = new mysqli("localhost", "root", "", "bd_proyecto2");
+
+	//Cogemos el nombre de usuario y la imagen de forma dinámica en la BD
+	$con =	"SELECT * FROM `tbl_usuario` WHERE `usu_id` = '". $_SESSION["usu_id"] ."'";
+	//echo $con;
+		//Lanzamos la consulta a la BD
+		$result	=	mysqli_query($mysqli,$con);
+		while ($fila = mysqli_fetch_row($result)) 
+			{
+				$usu_nickname	=	$fila[1];
+				$usu_img	=	$fila[6];
+				
+			}
+			
+	?>
+		<div class="header">
+			<div class="logo">
+				<a href="#"></a>
+			</div>
+			<div class="profile">
+			<p class="welcome">Hola bienvenido, <br /><b>
+			<?php echo $usu_nickname; ?></b></p>
+			</div>
+			
+			<h1>Reserva tus recursos</h1>
+
+		</div>
+		<nav>
+			<ul class="topnav">	
+				<li class="li"><a href="recursos.php">Recursos</a></li>
+				<li class="li"><a href="#">Mis recursos</a></li>
+				<li class="li"><a href="historial.php">Historial de recursos</a></li>
+			</ul>
+		</nav>
+		<div class="container">
+			<p class="reserved"> Tus recursos reservados són:</p> <br/>
+			
+				
+			</div>
+			<?php
+				//Seleccionamos todas las reservas que tiene asignado nuestro usuario
+				$con 	=	"SELECT * FROM `tbl_reserva` WHERE `res_usuarioid` = " . $_SESSION["usu_id"] . "";
+				$result 	=	mysqli_query($mysqli,$con);
+				while($fila	=	mysqli_fetch_row($result)){
+					//Extraemos los ID de los recursos
+					//echo $fila[3] . "<br/>";
+						//Seleccionamos los recursos correspodientes a las reservas del usuario
+						$con_rec	=	"SELECT * FROM `tbl_recurso` WHERE `rec_id` = ".$fila[3]." AND 'res_fechafinal IS NULL";
+						$result_rec 	=	mysqli_query($mysqli,$con_rec);
+						echo "<div class='content_rec'>";
+							while($fila_rec	=	mysqli_fetch_row($result_rec)){
+									//echo $fila[0]
+								echo "<table border>";
+									echo "<tr>";
+										echo "<td colspan='2'>" . $fila_rec[1] . "</td>";
+									echo "</tr>";
+									echo "<tr>";
+										echo "<td rowspan='3'><img class='img_recu' src='../img/".$fila_rec[2]."'></td>";
+										echo "<td>".$fila_rec[3]."</td>";
+									echo "</tr>";
+									echo "<tr>";
+										echo "<td>Fecha de inicio: " . date("d-m-Y",strtotime($fila[1])) . "</td>";
+									echo "</tr>";
+									echo "<tr>";
+										if(isset($fila[2])){
+										echo "<td>Fecha de Final: ".date("d-m-Y",strtotime($fila[2]))."</td>";
+									}
+									else{
+										echo "<td>En curso</td>";
+									}
+									echo "</tr>";
+										//echo $fila[2];
+									echo "<tr>";
+													
+											echo "<td colspan='2'> <a class='free_recu' href='liberar.proc.php?recu_id=".$fila_rec[0]."&res_id=".$fila[0]."'onclick='return destroy();'>LIBERAR RECURSO </a></td>";
+										
+										echo "</tr>";
+								echo "</table>";
+							}
+						echo "</div>";
+					
+				}
+
+			?>
+		</div>
+	</body>
+</html>
---
+++ proyecto2/php/recursos.php
@@ -0,0 +1,130 @@
+<?php
+		//realizamos la conexión
+		$conexion = mysqli_connect('localhost', 'root', '', 'bd_proyecto2');
+
+		//le decimos a la conexión que los datos los devuelva diréctamente en utf8, así no hay que usar htmlentities
+		$acentos = mysqli_query($conexion, "SET NAMES 'utf8'");
+
+		if (!$conexion) {
+		    echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
+		    echo "errno de depuración: " . mysqli_connect_errno() . PHP_EOL;
+		    echo "error de depuración: " . mysqli_connect_error() . PHP_EOL;
+		    exit;
+		}
+
+		session_start();
+		$mysqli = new mysqli("localhost", "root", "", "bd_proyecto2");
+		//Cogemos el nombre de usuario y la imagen de forma dinámica en la BD
+		$con =	"SELECT * FROM `tbl_usuario` WHERE `usu_id` = '". $_SESSION["usu_id"] ."'";
+		//echo $con;
+		//Lanzamos la consulta a la BD
+		$result	=	mysqli_query($mysqli,$con);
+		while ($fila = mysqli_fetch_row($result)) 
+			{
+				$usu_nickname	=	$fila[1];
+				$usu_img	=	$fila[6];
+			}
+			
+
+
+		$sql = "SELECT * FROM tbl_tiporecurso ORDER BY tr_id";
+
+		$disponible =	" SELECT * FROM tbl_recurso INNER JOIN tbl_tiporecurso ON tbl_tiporecurso.tr_id = tbl_recurso.rec_tipoid WHERE rec_estado='disponible' ";
+
+		$ocupado =	" SELECT * FROM tbl_recurso INNER JOIN tbl_tiporecurso ON tbl_tiporecurso.tr_id = tbl_recurso.rec_tipoid WHERE rec_estado='ocupado' ";
+
+		extract($_REQUEST);
+
+		if(isset($enviar)){
+		 	if($tr_id>0){
+		 		$disponible .= " AND rec_tipoid='$tr_id '";
+		 		$ocupado .= " AND rec_tipoid='$tr_id' ";
+		 	}
+		}
+
+		$tipos = mysqli_query($conexion, $sql);
+		$recursos = mysqli_query($conexion, $disponible);
+		$recursos1 = mysqli_query($conexion, $ocupado);
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="../css/recursos.css">
+	<title>Recursos</title>
+</head>
+<body>
+	<div class="header">
+			<div class="logo">
+				<a href="#"></a>
+			</div>
+			<div class="profile">
+			<p class="welcome">Hola bienvenido, <br /><b>
+			<?php echo $usu_nickname; ?></b></p>
+			</div>
+			
+			<h1>Reserva tus recursos</h1>
+
+	</div>
+	<nav>
+		<ul class="topnav">	
+			<li class="li"><a href="#">Recursos</a></li>
+			<li class="li"><a href="misrecursos.php">Mis recursos</a></li>
+			<li class="li"><a href="historial_recursos.php">Historial de recursos</a></li>
+		</ul>
+	</nav>
+<?php
+if(mysqli_num_rows($tipos)>0){
+	?>
+<form action="recursos.php" method="get" class="formtipo">
+	Tipo de recurso:
+	<select name="tr_id">
+		<option value="0">-- Elegir tipo --</option>
+		<?php
+				while($tipo=mysqli_fetch_array($tipos)){
+					echo "<option value=" . $tipo['tr_id'] . ">" . $tipo['tr_nombre'] . "</option>";
+				}
+			?>
+	</select>
+	<input type="submit" name="enviar" value="Filtrar">
+</form>
+<br/><br/>
+<h1>Recursos Disponibles</h1>
+<br/>
+<?php
+	}
+	if(mysqli_num_rows($recursos)>0){
+		while($recurso=mysqli_fetch_array($recursos)){
+					echo "Nombre: " .$recurso['rec_nombre']. "<br/>" ;
+					echo "Descripcion: " .$recurso['rec_descripcion']. "<br/>" ;
+					echo "Estado: " .$recurso['rec_estado']. "<br/><br/>";
+					echo "<a href='recursos.proc.php?rec_id=" .$recurso['rec_id']. "'>Reservar</a>";
+					echo "<br/>";
+					echo "<hr/>";
+			} 
+		} else {
+			echo "No hay recursos disponibles";
+		}
+
+	?>
+
+<br/><br/>
+<h1>Recursos en uso</h1>
+<br/>
+<?php
+	if(mysqli_num_rows($recursos1)>0){
+		while($recurso1=mysqli_fetch_array($recursos1)){
+					echo "Nombre: " .$recurso1['rec_nombre']. "<br/>" ;
+					echo "Descripcion: " .$recurso1['rec_descripcion']. "<br/>" ;
+					echo "Estado: " .$recurso1['rec_estado']. "<br/>" ;
+					echo "<hr/>";
+			} 
+		} else {
+			echo "No hay recursos en uso";
+		}
+
+	?>
+
+</body>
+</html>
